b938332b6f69ae98a10a0a1e6009999c25af1f0f
zaphkielyan <86876995+zaphkielyan@users.noreply.github.com>
2025-04-07 13:47:42 +0800

    Add files via upload

---
+++ Coursework_2_submission_template.m
@@ -0,0 +1,25 @@
+% Insert name here
+% Insert email address here
+
+
+%% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [10 MARKS]
+
+% Insert answers here
+
+%% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+
+% Insert answers here
+
+%% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
+
+% Insert answers here
+
+
+%% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [25 MARKS]
+
+% Insert answers here
+
+
+%% TASK 4 - REFLECTIVE STATEMENT [5 MARKS]
+
+% Insert answers here
