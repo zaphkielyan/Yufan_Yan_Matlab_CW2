7cfab5a733ad17487c5daba2f430f4298a44f93c
zaphkielyan <2676311890@qq.com>
2025-04-07 20:43:27 +0800

    v0.3 TASK 1 was finished successfully.

--- Yufan_Yan_20616136.m
+++ Yufan_Yan_20616136.m
@@ -24,8 +24,71 @@ for i = 1:10
 end
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+clear
+% Connect to Arduino
+a = arduino('COM3', 'Uno');
 
-% Insert answers here
+% Set acquisition duration to 601 seconds (to include minute 10 exactly)
+duration = 601;
+
+% Initialize temperature array
+temperature = zeros(1, duration);
+
+% Read and store temperature data every second
+for i = 1:duration
+    voltage = readVoltage(a, 'A0');       % Read analog voltage from A0
+    temp = (voltage - 0.5) * 100;         % Convert voltage to temperature using MCP9700A formula
+    temperature(i) = temp;                % Store temperature value
+    pause(1);                             % Wait 1 second
+end
+
+% Calculate statistical values
+maxTemp = max(temperature);
+minTemp = min(temperature);
+avgTemp = mean(temperature);
+
+% Plot temperature vs time graph
+time = 0:duration-1;  % Time vector in seconds
+plot(time, temperature);
+xlabel('Time (s)');              % Label for x-axis
+ylabel('Temperature (°C)');      % Label for y-axis
+title('Cabin Temperature vs Time');  % Graph title
+
+% Print formatted information to command window
+fprintf('Data logging initiated - 5/3/2024\n');
+fprintf('Location - Nottingham\n\n');
+
+for i = 0:10
+    index = i * 60 + 1;  % Index for each minute (0 to 10)
+    fprintf('Minute\t%d\n', i);
+    fprintf('Temperature\t%.2f C\n\n', temperature(index));
+end
+
+fprintf('Max temp\t%.2f C\n', maxTemp);
+fprintf('Min temp\t%.2f C\n', minTemp);
+fprintf('Average temp\t%.2f C\n\n', avgTemp);
+fprintf('Data logging terminated\n');
+
+% Write the same information to a text file
+fileID = fopen('cabin_temperature.txt', 'w');  % Open file in write mode
+
+fprintf(fileID, 'Data logging initiated - 5/3/2024\n');
+fprintf(fileID, 'Location - Nottingham\n\n');
+
+for i = 0:10
+    index = i * 60 + 1;
+    fprintf(fileID, 'Minute\t%d\n', i);
+    fprintf(fileID, 'Temperature\t%.2f C\n\n', temperature(index));
+end
+
+fprintf(fileID, 'Max temp\t%.2f C\n', maxTemp);
+fprintf(fileID, 'Min temp\t%.2f C\n', minTemp);
+fprintf(fileID, 'Average temp\t%.2f C\n\n', avgTemp);
+fprintf(fileID, 'Data logging terminated\n');
+
+fclose(fileID);  % Close the file
+
+
 
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 
